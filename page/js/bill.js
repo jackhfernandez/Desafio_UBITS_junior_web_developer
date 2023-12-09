be4ed892d3ef5fcaa39667226d221c92dd4d50ee
jackhfernandez <132464970+jackhfernandez@users.noreply.github.com>
2023-12-09 15:54:17 -0500

    Solucionando el reto

--- page/js/bill.js
+++ page/js/bill.js
@@ -3,23 +3,29 @@
 
 // 1.1 obtener de los query params los valores del nombre y precio del plan comprado para esto
 // se puede utilizar el metodo URLSearchParams y el window.location.search
-const urlParams = ...;
-const userName = ...;
-const planName = ...;
-const planPrice = ...;
+const urlParams = new URLSearchParams(window.location.search);
+const userName = urlParams.get("name");
+const planName = urlParams.get("planName");
+const planPrice = urlParams.get("price");
 
 const init = async () => {
   // 1.2 inyecta los valores obtenidos en los `container_bill`.
-  const userNameElement = ...;
-  const planNameElement = ...;
-  const priceElement = ...;
-  const priceTotalElement = ...;
+  const userNameElement = document.querySelector("span.username");
+  userNameElement.innerHTML = userName;
+  const planNameElement = document.querySelector("span.name");
+  planNameElement.innerHTML = planName;
+  const priceElement = document.querySelector("span.price");
+  priceElement.innerHTML = planPrice;
+  const priceTotalElement = document.querySelector("span.total_price");
+  priceTotalElement.innerHTML = planPrice;
 
   // 1.3 inyecta la fecha actual en la fecha de la factura
-  const actualDate = ...;
-  const month = ...;
-  const year = ...;
-  const day = ...;
+  const actualDate = new Date();
+  const month = actualDate.getMonth();
+  const year = actualDate.getFullYear();
+  const day = actualDate.getDay();
+  const dateElement = document.querySelector("div.date");
+  dateElement.innerHTML =     `${year}/${month}/${day}`;
 };
 
 init();
